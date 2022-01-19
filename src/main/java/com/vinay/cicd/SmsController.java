5e7887295aae6da083097c49d38f176078b70cdd
bale.kumar <vinay0639>
2022-01-19 23:10:03 +0530

    jenkin tests

---
+++ src/main/java/com/vinay/cicd/SmsController.java
@@ -0,0 +1,7 @@
+package com.vinay.cicd;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SmsController {
+}
--- src/test/java/com/vinay/cicd/CicdBasicAppApplicationTests.java
+++ src/test/java/com/vinay/cicd/CicdBasicAppApplicationTests.java
@@ -1,13 +1,19 @@
 package com.vinay.cicd;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CicdBasicAppApplicationTests {
 
+	@Autowired
+	private SmsController controller;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(controller);
 	}
 
 }
