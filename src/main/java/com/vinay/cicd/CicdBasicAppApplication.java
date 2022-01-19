8343e91bca4695c2f8917550b45a41866a1487ff
bale.kumar <vinay0639>
2022-01-19 22:00:57 +0530

    intial commit

---
+++ src/main/java/com/vinay/cicd/CicdBasicAppApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdBasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdBasicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinay/cicd/CicdBasicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdBasicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
